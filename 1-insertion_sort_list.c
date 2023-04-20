b30d35567573c1d29a88ae6a28b96586ad69c71b
F-O-P <tayler.coon@gmail.com>
2023-04-20 15:44:04 -0700

    updating task 1 files

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,1 +1,12 @@
 #include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of integers in ascending order
+ * @list: a list of integers
+ * Return: a pointer to the sorted list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+    
+}
