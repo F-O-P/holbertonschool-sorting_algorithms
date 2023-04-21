e6f35f371af7af5e0f4ff1bef1cf477106b4759e
F-O-P <tayler.coon@gmail.com>
2023-04-21 09:35:36 -0700

    task 1 working draft

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,5 +8,30 @@
 
 void insertion_sort_list(listint_t **list)
 {
-    
+	listint_t *current, *next, *prev;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = *list;
+	while (current != NULL)
+	{
+		next = current->next;
+		while (current->prev != NULL && current->n < current->prev->n)
+		{
+			prev = current->prev;
+			prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = prev;
+			current->next = prev;
+			current->prev = prev->prev;
+			prev->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
+		}
+		current = next;
+	}
 }
