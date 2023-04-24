ba6e551190e8e43639b742b466fdb5f9d75b114b
F-O-P <tayler.coon@gmail.com>
2023-04-24 09:35:01 -0700

    setup for extra problems

--- 100-shell_sort.c
+++
@@ -1,9 +0,0 @@
-#include "sort.h"
-
-/**
- * cocktail_sort_list - sorts a doubly linked list of integers in ascending
- * order using the Cocktail shaker sort algorithm
- * @list: pointer to list of integers
- *
- * Return: void
- */
